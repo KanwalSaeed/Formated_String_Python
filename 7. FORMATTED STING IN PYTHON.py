8d63d4ad7d89d7316aa84f6382c3289dd8683e1c
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-04 22:56:31 +0500

    first code

---
+++ 7. FORMATTED STING IN PYTHON.py
@@ -0,0 +1,10 @@
+
+full_Name = "Muhammad Ayyan "
+qualification = "Master in Compuuter"
+cpga = "4.4"
+print(f"{full_Name} is {qualification} holding {cpga} CPGA.")
+
+full_Name = "Qaiser Raza"
+marks = "550"
+roll_Number = "FAI-17-MSC-020"
+print(f"Assalam-o-Alaikum, Congraturation {full_Name} Roll number {roll_Number}, you have got {marks} Marks.")
